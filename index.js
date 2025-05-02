139efd11aad5e74aa3c4d4427ad717ab07a35213
Suhaib Khan <suhaib0900@gmail.com>
2025-05-02 16:00:25 +0100

    Setup Done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 async function fetchTodos() {
   const allTodos = await fetch('https://jsonplaceholder.typicode.com/todos');
   const todos = await allTodos.json();
-  const slicedTodos = todos.slice(0, 10);
+  const slicedTodos = todos.slice(0, 5).map((todo) => todo.title);
+
   window.localStorage.setItem('todos', JSON.stringify(slicedTodos));
 }
 function loadTodos() {
@@ -13,19 +14,34 @@ function saveTodo(todo) {
   allTodos.push(todo);
   window.localStorage.setItem('todos', JSON.stringify(allTodos));
 }
-window.addEventListener('DOMContentLoaded', async (event) => {
-  //   await fetchTodos();
-  const addTodo = document.querySelector('#add-todo');
+function appendToList(todo) {
   const todoList = document.querySelector('#todo-list');
-  const todoInput = document.querySelector('#todo-input');
+  const li = document.createElement('li');
+  li.innerText = todo;
+  li.classList.add('todo-item');
+  todoList.appendChild(li);
+}
+async function renderList() {
+  await fetchTodos();
   const todos = loadTodos();
+  todos.forEach((todo) => {
+    appendToList(todo);
+  });
+}
+window.addEventListener('DOMContentLoaded', async (event) => {
+  await renderList();
+  const addTodo = document.querySelector('#add-todo');
+  const todoInput = document.querySelector('#todo-input');
   todoInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
       saveTodo(todoInput.value);
+      appendToList(todoInput.value);
+      todoInput.value = '';
     }
   });
   addTodo.addEventListener('click', () => {
     saveTodo(todoInput.value);
+    appendToList(todoInput.value);
+    todoInput.value = '';
   });
 });
-//
